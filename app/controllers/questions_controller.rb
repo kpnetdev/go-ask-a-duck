ddcd8aa14c5eeab0116d4b9cd663ee41ae76d2d1
laura & Nina Hwang <pair+llcossette.nihwang@devbootcamp.com>
2014-04-24 18:17:13 -0700

    create question form feature test passing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,19 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
   end
 
   def new
     @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question])
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
   end
 end
--- spec/features/questions_features_spec.rb
+++ spec/features/questions_features_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 
 describe "Questions" do
-
+  let!(:question) {FactoryGirl.create :question}
   context "#index" do
     it "should give us form when we click on create" do
       visit questions_path
       click_on "Ask the Duck"
       expect(page).to have_content "Title"
+    end
+
+    it "should list all questions" do
+      visit questions_path
+      expect(page).to have_content question.title
+    end
+  end
+
+  context "#new" do
+    it "should fill in a form" do
+      visit new_question_path
+      fill_in "Title", :with => question.title
+      fill_in "Body", :with => question.body
+      click_on "Create Question"
+      expect(page).to have_content question.title
     end
   end
 end
