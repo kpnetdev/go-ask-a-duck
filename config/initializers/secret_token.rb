80761978d70bdee46ed2341526769f866adb64e1
laura & Nina Hwang <pair+llcossette.nihwang@devbootcamp.com>
2014-04-24 17:14:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoAskADuck::Application.config.secret_token = 'a574e6c79cafdf6f7cad27ee18bfdaf31f45f5fc3a353bd0ee51afcbd4bd80b33771e1287d16ea36f832012dcf545a704c0b9562f501426eb5b69aada3a3ed02'
