e892c2c7c9d872a244297e9b410689a94b6935f4
laura & Nina Hwang <pair+llcossette.nihwang@devbootcamp.com>
2014-04-24 17:38:15 -0700

    questions migration created and feature test

---
+++ spec/features/questions_features_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Questions do
+
+  context "#index" do
+    it "should give us form when we click on create" do
+      visit questions_path
+      click_on "Ask the Duck"
+      expect(page).to have_content "Question"
+    end
+  end
+end
