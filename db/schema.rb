f3d3fe45b3086eabb9bf044ebf770b7e9f807faa
Apprentice <apprentice@dbc32.local>
2014-04-26 15:48:27 -0700

    adds timestamps to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140426221410) do
+ActiveRecord::Schema.define(:version => 20140426224501) do
 
   create_table "answers", :force => true do |t|
     t.string   "title"
@@ -30,9 +30,11 @@ ActiveRecord::Schema.define(:version => 20140426221410) do
   end
 
   create_table "users", :force => true do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
