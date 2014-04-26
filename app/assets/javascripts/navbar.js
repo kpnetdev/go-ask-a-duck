f4aedfb6a7332a4b3c1a638afbb96f062c573cf8
kpnetdev <kelley.puckett@gmail.com>
2014-04-26 12:28:09 -0700

    renames auth_specs to navbar_features_spec

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,26 +2,30 @@ var Login = {
   init: function(){
     $('a.nav-login').on('click', this.toggleVisibility);
   },
-
   toggleVisibility: function(e) {
     e.preventDefault();
     $('.login').toggleClass('visible');
   }
 }
 
-
-
-
 var SignUp = {
   init: function() {
     $('a.nav-signup').on('click', this.toggleVisibility);
   },
-
   toggleVisibility: function(e) {
     e.preventDefault();
     $('.sign-up').toggleClass('visible');
   }
 }
+
+
+
+var Navbar = {
+  loginElement : $('a.nav-login')
+  signupElement : $('a.nav-signup')
+}
+
+
 
 
 
