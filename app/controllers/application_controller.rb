ae20d6e77b09e31a466318ef0bc66ca4c670d3d9
Apprentice <apprentice@dbc32.local>
2014-04-26 15:42:12 -0700

    moves current_user from sessions_helper to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+
+
+
+private
+
+  def current_user
+    if session[:user_id]
+      User.find session[:user_id]
+    else
+      nil
+    end
+  end
 end
