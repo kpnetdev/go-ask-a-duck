3f3d92a9f0d16266960cdd24811d388bb6da2adf
Apprentice <apprentice@dbc32.local>
2014-04-27 15:14:50 -0700

    i dunno

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :title, :body
+  attr_accessible :title, :body, :question_id, :user_id
   validates :body, :question_id, :user_id, presence: true
   belongs_to :user
   belongs_to :question
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe AnswersController do
   let!(:question) {FactoryGirl.create(:question)}
+  let!(:user) { FactoryGirl.create(:user) }
   # let(:answer) {FactoryGirl.create(:answer)}
 
   context "#create" do
     it "creates with valid attributes" do
       expect {
-        post :create, answer: FactoryGirl.attributes_for(:answer)
-        # post :create, :question_id => question.id, :answer => FactoryGirl.attributes_for(:answer)
+        # post :create, answer: FactoryGirl.attributes_for(:answer)
+
+        post :create, :question_id => question.id, :user_id => user.id, :answer => FactoryGirl.attributes_for(:answer)
       }.to change { Answer.count }.by(1)
     end
 
