2836273a4b14a073e700dfefd7368061e56d8658
kpnetdev <kelley.puckett@gmail.com>
2014-04-27 18:42:24 -0700

    gets code ready to integrate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :username
+  attr_accessible :email, :username, :password
   validates_presence_of :password, :email, :on => :create
   has_secure_password
   has_many :questions
